b95fa5d5a96abe474ac02c9a29426245bd32da76
codejoncode <jonathanjholloway@gmail.com>
2018-12-22 11:15:01 -0600

    created some randomness for the game on each load

--- script.js
+++ script.js
@@ -5,16 +5,40 @@ let doorImage3 = document.getElementById("door3");
 const botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
 const beachDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg";
 const spaceDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg";
+let numClosedDoors = 3; 
+let openDoor1; 
+let openDoor2; 
+let openDoor3;
 
+randomChoreDoorGenerator = () => {
+    const choreDoor = Math.floor(Math.random() * numClosedDoors); 
+    
+    if(choreDoor === 1){
+        openDoor1 = botDoorPath;
+        openDoor2 = spaceDoorPath;
+        openDoor3 = beachDoorPath; 
+    }  else if (choreDoor === 2){
+        openDoor2 = botDoorPath;
+        openDoor1 = beachDoorPath;
+        openDoor3 = spaceDoorPath;
+    } else {
+        openDoor3 = botDoorPath;
+        openDoor1 = spaceDoorPath;
+        openDoor2 = beachDoorPath;
+    }
+}
+randomChoreDoorGenerator()
 
 doorImage1.onclick = () => {
-    doorImage1.src = botDoorPath; 
+    doorImage1.src = openDoor1; 
 };
 
 doorImage2.onclick = () => {
-    doorImage2.src = beachDoorPath;
+    doorImage2.src = openDoor2;
 };
 
 doorImage3.onclick = () => {
-    doorImage3.src = spaceDoorPath; 
+    doorImage3.src = openDoor3; 
 };
+
+
